82506fb0ebe85599eece9294cbca914ef7fc8245
doodalasudharshana <sudadudala@gmail.com>
2024-06-16 16:43:31 +0530

    Second test

---
+++ src/reaact-fundamentals/components/Header/Table.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+ import "./Table.css";
+
+export const Table = () => {
+     return (
+        <div>
+      <h1 className="table tags">Practice on Table Tags</h1>;
+        <table>
+            <caption>Details of Flowers</caption>
+            <tbody>
+            <tr>
+                <td> Rose</td>
+            <td>Lilly</td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>20</td>
+            </tr>
+            </tbody>
+        </table>
+     </div>
+    );
+};
---
+++ src/reaact-fundamentals/components/Header/Tags/Tags.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./tags.css";
+
+export const Tags = () => {
+//   return <h1 className="basic tags">Practice on HTML Tags</h1>;
+return (
+  <div>
+    <h1>Sudarshana</h1><br/>
+    <del>Sudarshana</del>
+    <strong>Sudarshana</strong>
+    <br/>
+    <small>Sudarshana</small>
+    <underlined>Sudarshana</underlined>
+    <i>Sudarshana</i>
+    <p>I am practicing HTML Tags </p>
+    <p>Hi <q>Chitty</q></p>
+
+    <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fdiscover%2Ffree-nature-images&psig=AOvVaw29Fe50Cri0ty1vioep6yJ-&ust=1718608160969000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjmn7nI34YDFQAAAAAdAAAAABAE">I am anchor Tag</a>
+    
+  </div>
+)  
+}
