00b325b44d692702e1bdedfbd98817440922c4a7
Hemanthkatam <katamkv@gmail.com>
2024-06-16 16:59:50 +0530

    props basics

--- src/reaact-fundamentals/ReactFundamentals.js
+++ src/reaact-fundamentals/ReactFundamentals.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
 import { Tags } from "./components/Header/Tags/Tags";
- import { Table } from "./components/Header/Table";
+import { Table } from "./components/Header/Table";
 //  import { Footer} from ".components/Header/Footer";
-import { Content} from "./components/Content/Content"
-import { Footer} from "./components/Header/Footer";
+import { Content } from "./components/Content/Content";
+import { Footer } from "./components/Header/Footer";
 export const ReactFundamentals = () => {
+  const header = "I am header from ReactFundamentals";
+
   return (
     <div>
-      <Header />
- <Tags />
+      <Header
+        headerMessage={header}
+        anotherMaggese="another message"
+        age={25}
+      />
+      <Tags />
       <Table />
       <Footer />
       <Content />
     </div>
   );
 };
+// components will communiate in props
 
 // TODO
 
