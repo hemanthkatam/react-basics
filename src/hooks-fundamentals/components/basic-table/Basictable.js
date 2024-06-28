803e07c8e0f0a0d022c8ac30d1437c73b4aeb85c
Hemanthkatam <katamkv@gmail.com>
2024-06-28 20:14:22 +0530

    test

--- src/hooks-fundamentals/components/basic-table/Basictable.js
+++ src/hooks-fundamentals/components/basic-table/Basictable.js
@@ -3,10 +3,13 @@ import React, { useState, useEffect } from "react";
 import "./Basictable.css";
 
 // useState -stores data which caiuese for rerender
-const URL = "https://run.mocky.io/v3/50469286-d0ca-4ce1-a9ce-88eae349dc76";
+const URL = "https://mocki.io/v1/60c041a4-5380-4d3f-9afe-38985e642368";
 
 export const Basictable = () => {
   const [empData, setEmpData] = useState([]); //on mounting
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [salary, setSalary] = useState("");
   const headers = ["first_name", "last_name", "salary"];
 
   const getEmpDataFromServer = async () => {
@@ -28,8 +31,50 @@ export const Basictable = () => {
     setEmpData(updatedDaAta);
   };
 
+  const onFirstnameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+  const onLastnameChange = (event) => {
+    setLastname(event.target.value);
+  };
+  const onSalaryChange = (event) => {
+    setSalary(event.target.value);
+  };
+
+  const onSubmit = () => {
+    const data = {
+      first_name: firstname,
+      last_name: lastname,
+      salary: salary,
+    };
+    console.log("---data", data);
+    const updatedData = [...empData, data];
+    setEmpData(updatedData);
+  };
+
   return (
     <div className="tableMain">
+      <div>
+        <div>
+          <div>First name:</div>
+          <div>
+            <input type="textbox" onChange={onFirstnameChange} />
+          </div>
+        </div>
+        <div>
+          <div>last name:</div>
+          <div>
+            <input type="textbox" onChange={onLastnameChange} />
+          </div>
+        </div>
+        <div>
+          <div>Salary:</div>
+          <div>
+            <input type="textbox" onChange={onSalaryChange} />
+          </div>
+        </div>
+        <button onClick={onSubmit}>submit</button>
+      </div>
       <table>
         <thead>
           {headers.map((test) => (
