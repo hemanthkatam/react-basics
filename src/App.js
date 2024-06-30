83cd802a17ebd4aa6eb0af5c7c14b6ad76e1856d
doodalasudharshana <sudadudala@gmail.com>
2024-06-30 23:56:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { ReactFundamentals } from "./reaact-fundamentals/ReactFundamentals";
 import "./App.css";
 import { HooksFundaments } from "./hooks-fundamentals/HooksFundamentals";
+import { BillingTable } from "./billing-table/components/BillingTable";
 
 function App() {
   return (
     <div className="App">
       {/* <ReactFundamentals /> */}
-      <HooksFundaments />
+      {/* <HooksFundaments /> */}
+      <BillingTable />
     </div>
   );
 }
---
+++ src/billing-table/components/BillingTable.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+ //import "./BillingTable.css";
+ 
+export  const BillingTable =()=>{
+    const [item, updatedItem]=useState([])
+    const [itemName, updatedItemName] = useState("");
+    const [price, updatedPrice] = useState("");
+    const [itemQuantity, updatedItemQuantity] = useState("");
+    const [amount, updatedAmount] = useState("");
+    const headers=["itemName","price","itemQuantity","amount"]
+    
+    // const onItemChange=(event)=>{
+    //     updatedItem(event.target.value)
+    // } 
+
+    const onItemNameChange=(event)=>{
+        updatedItemName(event.target.value)
+    }
+    const onPriceChange=(event)=>{
+        updatedPrice(event.target.value)
+    }
+    const onItemQuantityChange=(event)=>{
+        updatedItemQuantity(event.target.value)
+    }
+    const onAmountChange=(event)=>{
+        updatedAmount(event.target.value)
+    }
+
+    let onSubmit=()=>{
+        const data={
+            itemName:itemName,
+            price:price,
+            itemQuantity:itemQuantity,
+            amount:amount
+        };
+        console.log("....details....",data)
+        const updatedData=[...item,data]
+        updatedItem(updatedData)
+        updatedItemName('')
+        updatedPrice('')
+        updatedItemQuantity('')
+        updatedAmount('')
+
+    };
+
+    return(
+        <div class="Chitty">
+
+            <div>
+                <div>itemName:</div>
+                <div>
+                    <input type="textbox" onChange={onItemNameChange}></input>
+                </div>
+            </div>
+            <div>
+                <div>price:</div>
+                <div>
+                    <input type="textbox" onChange={onPriceChange}></input>
+                </div>
+            </div>
+            <div>
+                <div>itemQuantity:</div>
+                <div>
+                    <input type="textbox" onChange={onItemQuantityChange}></input>
+                </div>
+            </div>
+            <div>
+                <div>amount:</div>
+                <div>
+                    <input type="textbox" onChange={onAmountChange}></input>
+                </div>
+            </div>
+            <div>
+            <button onClick={onSubmit} disabled={!itemName || !price || !itemQuantity || !amount}>Submit</button>
+            </div>
+            <table>
+                <thead>
+                {headers.map((test) => (
+                  <th>{test}</th>
+                 ))}
+                </thead>
+                 
+                <tbody>
+                  {item.map((test) => {
+                    return(
+                            <tr>
+                            <td>{test.itemName}</td>
+                            <td>{test.price}</td>
+                            <td>{test.itemQuantity}</td>
+                            <td>{test.amount}</td>
+                            </tr>
+                    )
+                  })}
+                    
+                </tbody>
+            </table>
+        </div>
+        
+    )
+}
